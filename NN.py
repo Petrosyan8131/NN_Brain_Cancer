2ec7285bc5e2649d301d66e1cb16efc0e159530e
Petrocyan8131 <sokolovpv@inbox.ru>
2025-05-04 21:40:52 +0300

    fixes

--- NN.py
+++ NN.py
@@ -12,7 +12,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import RandomOverSampler, SMOTE
-# import shap
+import shap
 
 # импорт и подготовка датасета
 df = pd.read_csv('data/scaled_train.csv')
@@ -35,37 +35,38 @@ X_res, Y_res = smote(X, Y)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X_res, Y_res, test_size=0.2)
 data = X_train, X_test, Y_train, Y_test
+data_tr = X_train, Y_train
 
 # используемые модели
 def KNN(data):
-    data_train, data_test, res_train, res_test = data
+    data_train, res_train = data
     model_neighbor = KNeighborsClassifier(n_neighbors=5, leaf_size=15, p=2)
     res_train = res_train.values.ravel()
     model_neighbor = model_neighbor.fit(data_train, res_train)
     return model_neighbor
 
 def SVM(data):
-    data_train, data_test, res_train, res_test = data
-    clf = SVC(kernel='rbf', C=2.0, cache_size=200, degree=3, gamma=0.4, max_iter=300, tol=0.1)
+    data_train, res_train = data
+    clf = SVC(kernel='rbf', C=2.0, cache_size=200, degree=3, gamma=0.4, max_iter=200, tol=0.1)
     res_train = res_train.values.ravel()
     clf = clf.fit(data_train, res_train)
     return clf
 
 def DecisionTree(data):
-    data_train, data_test, res_train, res_test = data
+    data_train, res_train = data
     dt = DecisionTreeClassifier(random_state=0, max_depth=5, max_features=7, max_leaf_nodes=20, min_samples_leaf=1, min_samples_split=4)
     dt = dt.fit(data_train, res_train)
     return dt
 
 def LR(data):
-    data_train, data_test, res_train, res_test = data
+    data_train, res_train = data
     lr = LogisticRegression(C=5, intercept_scaling=1.0, max_iter=100)
     res_train = res_train.values.ravel()
     lr = lr.fit(data_train, res_train)
     return lr
 
 def RandomForest(data):
-    data_train, data_test, res_train, res_test = data
+    data_train, res_train = data
     rf = RandomForestClassifier(max_features=5, n_estimators=100, min_samples_leaf=1, min_samples_split=2)
     res_train = res_train.values.ravel()
     rf = rf.fit(data_train, res_train)
@@ -75,31 +76,36 @@ def RandomForest(data):
 def balanced_accuracy(model, data_test, res_test):
     res_pred = model.predict(data_test)
     accuracy = balanced_accuracy_score(res_test, res_pred)
-    return accuracy, res_pred
+    return accuracy
 
 def f1_accuracy(model, data_test, res_test):
     res_pred = model.predict(data_test)
-    accur = f1_score(res_test, res_pred, average="binary")
-    return accur, res_pred
+    accuracy = f1_score(res_test, res_pred, average="binary")
+    return accuracy
 
 def fb_accuracy(model, data_test, res_test):
     res_pred = model.predict(data_test)
-    accur = fbeta_score(res_test, res_pred, average="binary", beta=2)
-    return accur, res_pred
+    accuracy = fbeta_score(res_test, res_pred, average="binary", beta=2)
+    return accuracy
+
+knn_pred = KNN(data_tr).predict(X_test)
+svm_pred = SVM(data_tr).predict(X_test)
+dt_pred = DecisionTree(data_tr).predict(X_test)
+lr_pred = LR(data_tr).predict(X_test)
+rf_pred = RandomForest(data_tr).predict(X_test)
+
 
 # таблица полученных результатов
-predict_data = (balanced_accuracy(KNN(data), X_test, Y_test)[1], balanced_accuracy(SVM(data), X_test, Y_test)[1], 
-       balanced_accuracy(DecisionTree(data), X_test, Y_test)[1], balanced_accuracy(LR(data), X_test, Y_test)[1], 
-       balanced_accuracy(RandomForest(data), X_test, Y_test)[1])
-b_acc = [balanced_accuracy(KNN(data), X_test, Y_test)[0], balanced_accuracy(SVM(data), X_test, Y_test)[0], 
-       balanced_accuracy(DecisionTree(data), X_test, Y_test)[0], balanced_accuracy(LR(data), X_test, Y_test)[0], 
-       balanced_accuracy(RandomForest(data), X_test, Y_test)[0]]
-f1_acc = [f1_accuracy(KNN(data), X_test, Y_test)[0], f1_accuracy(SVM(data), X_test, Y_test)[0], 
-          f1_accuracy(DecisionTree(data), X_test, Y_test)[0], f1_accuracy(LR(data), X_test, Y_test)[0], 
-          f1_accuracy(RandomForest(data), X_test, Y_test)[0], ]
-fb_acc = [fb_accuracy(KNN(data), X_test, Y_test)[0], fb_accuracy(SVM(data), X_test, Y_test)[0], 
-          fb_accuracy(DecisionTree(data), X_test, Y_test)[0], fb_accuracy(LR(data), X_test, Y_test)[0], 
-          fb_accuracy(RandomForest(data), X_test, Y_test)[0], ]
+predict_data = (knn_pred, svm_pred, dt_pred, lr_pred, rf_pred)
+b_acc = [balanced_accuracy(KNN(data_tr), X_test, Y_test), balanced_accuracy(SVM(data_tr), X_test, Y_test), 
+       balanced_accuracy(DecisionTree(data_tr), X_test, Y_test), balanced_accuracy(LR(data_tr), X_test, Y_test), 
+       balanced_accuracy(RandomForest(data_tr), X_test, Y_test)]
+f1_acc = [f1_accuracy(KNN(data_tr), X_test, Y_test), f1_accuracy(SVM(data_tr), X_test, Y_test), 
+          f1_accuracy(DecisionTree(data_tr), X_test, Y_test), f1_accuracy(LR(data_tr), X_test, Y_test), 
+          f1_accuracy(RandomForest(data_tr), X_test, Y_test), ]
+fb_acc = [fb_accuracy(KNN(data_tr), X_test, Y_test), fb_accuracy(SVM(data_tr), X_test, Y_test), 
+          fb_accuracy(DecisionTree(data_tr), X_test, Y_test), fb_accuracy(LR(data_tr), X_test, Y_test), 
+          fb_accuracy(RandomForest(data_tr), X_test, Y_test), ]
 fn = [confusion_matrix(Y_test, predict_data[0])[1][0], confusion_matrix(Y_test, predict_data[1])[1][0],
       confusion_matrix(Y_test, predict_data[2])[1][0], confusion_matrix(Y_test, predict_data[3])[1][0],
       confusion_matrix(Y_test, predict_data[4])[1][0]]
@@ -116,7 +122,7 @@ res = pd.DataFrame({
 title = f"Confusion matrix for {DecisionTree.__name__}, without normalization"
 
 disp = ConfusionMatrixDisplay.from_estimator(
-    DecisionTree(data),
+    DecisionTree(data_tr),
     X_test,
     Y_test,
     cmap=plt.cm.Blues,
@@ -128,6 +134,10 @@ disp.ax_.set_title(title)
 print('-'*60)
 print(res)
 print('-'*60)
+
+expl_rf = shap.Explainer(RandomForest(data_tr))
+shap_values = expl_rf(X_test)
+print(shap_values)
 
 # plt.show()
 
